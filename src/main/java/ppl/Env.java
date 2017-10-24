eb784a69848610803532e9085850bdf9ab36db0a
sky91 <feitiandaxia1991@163.com>
2017-10-24 23:15:42 +0800

    worked

---
+++ src/main/java/ppl/Env.java
@@ -0,0 +1,29 @@
+package ppl;
+
+import java.util.Objects;
+
+public class Env {
+	private final String k;
+	private final int v;
+	private final Env parentEnv;
+
+	public Env(String k, int v, Env parentEnv) {
+		this.k = k;
+		this.v = v;
+		this.parentEnv = parentEnv;
+	}
+
+	public int getValue(String name) {
+		Integer value = null;
+		if(Objects.equals(name, k)) {
+			return v;
+		}
+		if(parentEnv != null) {
+			value = parentEnv.getValue(name);
+		}
+		if(value == null) {
+			throw new RuntimeException("Cannot find value for [" + name + "]");
+		}
+		return value;
+	}
+}
--- src/main/java/ppl/main.java
+++ src/main/java/ppl/main.java
@@ -22,7 +22,7 @@ public class main {
 		Environment env = new Environment();
 		LetLangExp LLe = new LetLangExp();
 		// LLe.tokens1 = sclist1;
-		int result = valueof(Lc, env);
+		int result = valueOf(Lc, null);
 		System.out.println(result);
 	}
 
@@ -61,5 +61,29 @@ public class main {
 			}
 		}
 		return 0;
+	}
+
+	public static int valueOf(LetLangExp exp, Env env) {
+		switch(exp.Vari) {
+			case let: {
+				int e1Value = valueOf(exp.getletexp1(), env);
+				Env subEnv = new Env(exp.getindetifier().getVar(), e1Value, env);
+				return valueOf(exp.getletexp2(), subEnv);
+			}
+			case minus: {
+				int a1 = valueOf(exp.getdiffexp1(), env);
+				int a2 = valueOf(exp.getdiffexp2(), env);
+				return a1 - a2;
+			}
+			case number: {
+				return exp.getnum();
+			}
+			case Var: {
+				return env.getValue(exp.getVar());
+			}
+			default: {
+				throw new RuntimeException("not implemented");
+			}
+		}
 	}
 }
